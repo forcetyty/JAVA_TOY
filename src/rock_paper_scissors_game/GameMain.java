35374e45f86f2b55db31561f62a1a3348f0dd6a2
taeyoung <forcetyty@gmail.com>
2024-10-09 23:30:48 +0900

    모드 추가

--- src/rock_paper_scissors_game/GameMain.java
+++ src/rock_paper_scissors_game/GameMain.java
@@ -10,6 +10,20 @@ import java.util.Scanner;
 public class GameMain {
     public static void main(String[] args) {
 
+        Scanner scanSelect = new Scanner(System.in);
+
+        //초기 메뉴 구성
+        System.out.println("*********************************************");
+        System.out.println("*************** 가위 바위 보 게임 ***************");
+        System.out.println("*********************************************");
+
+        System.out.println(" 게임 모드 : 1 [ 1 : 1 모드 ] / 2 [ 1 : N 모드 ] ");
+        System.out.print(" 선택하세요 : ");
+        int gameSel = scanSelect.nextInt();
+
+        System.out.println("********************** " + gameSel + " 모드를 선택 하셨습니다 **");
+
+        /*
         Scanner scanUser = new Scanner(System.in);
         int ranNum = (int)((Math.random()*3)+1);
         String computerUser = "";
@@ -53,7 +67,7 @@ public class GameMain {
             System.out.println(" >>>> 사용자가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
 
         }
-
+        */
 
     }
 }
