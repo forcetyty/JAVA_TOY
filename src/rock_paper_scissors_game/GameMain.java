6b20ba16a3d9fc5ed3ac1119a9599373b213ea97
taeyoung <forcetyty@gmail.com>
2024-10-11 21:30:35 +0900

    기능추가

--- src/rock_paper_scissors_game/GameMain.java
+++ src/rock_paper_scissors_game/GameMain.java
@@ -22,7 +22,7 @@ public class GameMain {
         int gameSel = scanSelect.nextInt();
 
         System.out.println("********************** " + gameSel + " 모드를 선택 하셨습니다 **");
-
+        game(gameSel);
 
 
         /*
@@ -73,8 +73,58 @@ public class GameMain {
     }
 
     // 게임 실행 메소드
-    public static int game(){
-        
+    public static int game(int type){
+
+        if(type == 1){
+            Scanner scanUser = new Scanner(System.in);
+            int ranNum = (int)((Math.random()*3)+1);
+            String computerUser = "";
+            int userNum = 0;
+
+            if(ranNum == '1'){
+                computerUser = "가위";
+
+            }else if(ranNum == '2'){
+                computerUser = "바위";
+
+            }else{
+                computerUser = "보";
+
+            }
+
+            // 가위바위보 게임 기본틀
+            System.out.println(" >>>>>>>>> 가위 바위 보 게임");
+            System.out.print(" >>>> 가위, 바위, 보 입력 : ");
+            String user = scanUser.nextLine();
+
+            if(user.equals(computerUser)){
+                System.out.println(" >>>> 비겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("가위") && computerUser.equals("바위")){
+                System.out.println(" >>>> 컴퓨터가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("가위") && computerUser.equals("보")) {
+                System.out.println(" >>>> 사용자가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("바위") && computerUser.equals("가위")){
+                System.out.println(" >>>> 사용자가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("바위") && computerUser.equals("보")){
+                System.out.println(" >>>> 컴퓨터가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("보") && computerUser.equals("가위")){
+                System.out.println(" >>>> 컴퓨터가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }else if(user.equals("보") && computerUser.equals("바위")){
+                System.out.println(" >>>> 사용자가 이겼습니다 - 사용자 " + user + " - 컴퓨터 " + computerUser);
+
+            }
+
+        }else if( type == 2){
+
+        }
+
+
         return 0;
     }
 }
